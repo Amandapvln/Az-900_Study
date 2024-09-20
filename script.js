f86183a219abda500289b29bf1d160e6db63c617
Amanda Paiva Lino <106124434+Amandapvln@users.noreply.github.com>
2024-09-20 16:33:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 // Aqui você pode adicionar funcionalidades interativas usando JavaScript
 console.log("Portfólio carregado com sucesso!");
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  alert('Mensagem enviada com sucesso!');
+});
